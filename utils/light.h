54b856a30f43b42b38a0760f4045998e79987279
adelep <adele.pluquet@epita.fr>
2022-02-24 19:53:22 +0100

    [ADD] Empty Light class.

---
+++ utils/light.h
@@ -0,0 +1,17 @@
+/**
+ * @file light.h
+ * @author Adèle PLUQUET and Adrien ANTON LUDWIG
+ * @brief Light class.
+ * @version 0.1
+ * @date 2022-02-24
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef UTILS_LIGHT_H_
+#define UTILS_LIGHT_H_
+
+class Light {};
+
+#endif  // UTILS_LIGHT_H_
