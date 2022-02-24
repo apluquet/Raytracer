0b7c2b9a6a7b8ab0f0443f59326db21173f0ce80
adelep <adele.pluquet@epita.fr>
2022-02-24 19:54:30 +0100

    [ADD] Empty Camera class

---
+++ utils/camera.h
@@ -0,0 +1,17 @@
+/**
+ * @file camera.h
+ * @author Adèle PLUQUET and Adrien ANTON LUDWIG
+ * @brief Camera class.
+ * @version 0.1
+ * @date 2022-02-24
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef UTILS_CAMERA_H_
+#define UTILS_CAMERA_H_
+
+class Camera {};
+
+#endif  // UTILS_CAMERA_H_
