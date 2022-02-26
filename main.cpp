69969f73f1b723325fd85aea75c7a12c85bf8fab
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-02-26 18:16:10 +0100

    [ADD] multiple objects management

--- main.cpp
+++ main.cpp
@@ -24,38 +24,53 @@ int main() {
   Color pink(255, 51, 153);
   Uniform_Texture material_pink = Uniform_Texture(pink);
 
+  Color sth(150, 10, 20);
+  Uniform_Texture material_sth = Uniform_Texture(sth);
+
   // Create a sphere
-  Point sphere_center = Point(0, 0, 10);
+  Point sphere_center = Point(0, 0, 0);
   double sphere_radius = 4.2;
   Sphere sphere = Sphere(sphere_center, sphere_radius, &material_pink);
+
+  Point sphere2_center = Point(9, 7, 0);
+  double sphere2_radius = 2.1;
+  Sphere sphere2 = Sphere(sphere2_center, sphere2_radius, &material_sth);
 
   // Our image parameter
   int size = 1000;
   Image image(size, size);
 
   // Camera definition
-  Point position(0, 0, 0);
-  Vector direction(0, 0, 1);
-  Vector up(1, 0, 0);
+  Point position(0, -10, 10);
+  Vector direction = Vector(0, 0, 0) - position;
+  Vector up = Vector(0, position.y, 0) ^ direction;
   Camera camera(position, direction, up, 1, 90, 90, image);
   Scene scene(camera);
+  scene.addObject(&sphere2);
   scene.addObject(&sphere);
 
   Ray ray;
   std::optional<Point> intersection;
-
   for (int i = 0; i < image.width; i++)
     for (int j = 0; j < image.height; j++) {
       ray = camera.cast_ray(i, j);
 
-      for (Object* object : scene.objects)
+      double distance = INFINITY;
+      Color color(0, 0, 0);
+
+      for (Object* object : scene.objects) {
         intersection = object->intersect(ray);
+        if (!intersection.has_value()) continue;
 
-      if (!intersection.has_value()) continue;
+        double new_distance = (intersection.value() - ray.origin).length();
 
-      Color color = sphere.get_texture(intersection.value());
+        if (new_distance < distance) {
+          distance = new_distance;
+          color = object->get_texture(intersection.value());
+        }
+      }
 
-      image.my_image[i][j] = color;
+      if (distance != INFINITY) image.my_image[i][j] = color;
     }
 
   image.to_ppm();
