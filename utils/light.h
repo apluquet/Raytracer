c149bc07c4694d41436ce27ba95a87fb39754f41
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-03-03 10:35:38 +0100

    [ADD] Light : getters

--- utils/light.h
+++ utils/light.h
@@ -13,14 +13,27 @@
 #include "utils/color.h"
 #include "utils/point.h"
 
-class Light {};
-
-class PointLight : public Light {
+class Light {
  public:
-  PointLight(const Point &position, const Color &color, const double &intensity)
+  Light() {}
+  Light(const Point &position, const Color &color, const double &intensity)
       : position(position), color(color), intensity(intensity) {}
+
+  virtual Point get_position() = 0;
+  virtual Color get_color() = 0;
+  virtual double get_intensity() = 0;
 
   Point position;
   Color color;
   double intensity;
+};
+
+class PointLight : public Light {
+ public:
+  PointLight(const Point &position, const Color &color, const double &intensity)
+      : Light(position, color, intensity) {}
+
+  Point get_position() override { return position; }
+  Color get_color() override { return color; }
+  double get_intensity() override { return intensity; }
 };
