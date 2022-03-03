99c6827f9a38e06ed0440321178ca2ea361a3bda
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-03-03 11:14:07 +0100

    [UPD] init Image black

--- utils/image.h
+++ utils/image.h
@@ -20,10 +20,10 @@
 class Image {
  public:
   Image(std::size_t width, std::size_t height) : height(height), width(width) {
-    Color white = Color(255, 255, 255);
+    Color black = Color(0, 0, 0);
     for (std::size_t i = 0; i < height; i++) {
       std::vector<Color> line;
-      for (std::size_t j = 0; j < width; j++) line.push_back(white);
+      for (std::size_t j = 0; j < width; j++) line.push_back(black);
       my_image.push_back(line);
     }
   }
