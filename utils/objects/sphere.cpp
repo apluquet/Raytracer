07e0133d0a1c2888fe35f4d6ed986d137f3db366
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-03-08 09:21:04 +0100

    [FIX] Sphere intersection precision error

--- utils/objects/sphere.cpp
+++ utils/objects/sphere.cpp
@@ -11,24 +11,42 @@
 
 #include "utils/objects/sphere.h"
 
+#include <utility>
+
 // https://viclw17.github.io/2018/07/16/raytracing-ray-sphere-intersection/
 // Sphere : dot((P - C), (P - C)) = r²
 // Ray : p(t) = O + tD
 std::optional<Intersection> Sphere::intersect(const Ray &ray) {
+  if (round(ray.direction.length()) != 1) throw;
+  // else a = ray.direction * ray.direction = 1 * 1 * cos(0) = 1
+
   Vector diff = ray.origin - center;
-  double a = ray.direction * ray.direction;
+
   double b = ray.direction * 2 * diff;
   double c = diff * diff - radius * radius;
-  double discriminant = b * b - 4 * a * c;
 
-  if (discriminant < 0) return std::nullopt;
+  double discriminant = b * b - 4 * c;
 
-  double sqrt_discriminant = sqrt(discriminant);
-  double t = -b - sqrt_discriminant / 2 * a;
+  double t0, t1;
 
-  if (t < 0) t = -b + sqrt_discriminant / 2 * a;
+  if (discriminant < 0) {  // there is no solution
+    return std::nullopt;
+  } else if (discriminant == 0) {  // one solution
+    t0 = t1 = -0.5 * b;
+  } else {  // two solutions
+    double q = (b > 0) ? -0.5 * (b + sqrt(discriminant))
+                       : -0.5 * (b - sqrt(discriminant));
+    t0 = q;
+    t1 = c / q;
+    if (t1 < t0) std::swap(t0, t1);  // Sort t1 and t2
+  }
 
-  Point intersection_point = ray.origin + ray.direction * t;
+  if (t0 < 0) {                       // t0 is behind the ray
+    if (t1 < 0) return std::nullopt;  // t1 is also behind
+    t0 = t1;                          // t1 is in front of the ray
+  }
+
+  Point intersection_point = ray.origin + ray.direction * t0;
 
   Intersection intersection(ray, intersection_point,
                             normal(intersection_point));
