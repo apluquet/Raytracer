73ac4d9184374f1f83bea0c64b21f90a03812452
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-04-08 23:27:27 +0200

    [ADD] AABB object: class definition, cmake update

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 
 #include "utils/blob.h"
 #include "utils/image.h"
+#include "utils/materials/cartoon.h"
 #include "utils/materials/phong.h"
 #include "utils/object_loader.h"
 #include "utils/objects/sphere.h"
@@ -25,20 +26,21 @@
 int main(int argc, char* argv[]) {
   // Create materials
   Color red(1., 0., 0.);
-  Color green(0., 1., 0.);
-  PhongMaterial phong_material(green, 0.2, 0.5, 0.3, 100);
-  PhongMaterial phong_reflect(red, 0.2, 0.8, 0.2, 0.5, 100);
+  CartoonMaterial cartoon_material(red, 0.2, 0.5, 0.3, 100);
 
   // Get object from .obj
   std::vector<std::string> files(argv + 1, argv + argc);
-  std::vector<Object*> objects = object_loader(files, &phong_reflect);
+  std::vector<Object*> objects = object_loader(files, &cartoon_material);
 
   // Create scene (camera + light)
+  // PointLight light(Point(4.1, -4.32, 3.36), Color(1., 1., 1.), 1);
   PointLight light(Point(5, 5, 5), Color(1., 1., 1.), 1);
   Image image(1920 / 2, 1080 / 2);
   // Image image(480, 270);
-  Camera camera(Point(5, 0, 0), image, Vector(-1, 0, 0), Vector(0, 0, 1), 1,
-                120, 90);
+  // Camera camera(Point(3.56, 1.67, 3.87), image, Vector(0, -1, 0),
+  // Vector(0, 0, 1), 1, 60, 45);
+  Camera camera(Point(6, 0, 0), image, Vector(-1, 0, 0), Vector(0, 0, 1), 1, 60,
+                45);
   Scene scene(camera, 1.5, Color(0., 0., 0.));
   scene.addLight(&light);
 
---
+++ utils/objects/aabb.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file bouding_box.h
+ * @author Adèle PLUQUET and Adrien ANTON LUDWIG
+ * @brief Bounding box class method implementation.
+ * @version 0.1
+ * @date 2022-04-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "utils/objects/aabb.h"
