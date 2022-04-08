98299d542457777222dc6155ad3ece3d496fa349
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-04-09 00:10:39 +0200

    [UPD] AABB: add meterial to constructor

--- utils/objects/aabb.h
+++ utils/objects/aabb.h
@@ -20,8 +20,9 @@
 class AABB : public Object {
  public:
   AABB() {}
-  AABB(const Point &min_vertex, const Point &max_vertex)
-      : min_vertex(min_vertex), max_vertex(max_vertex) {}
+  AABB(const Point &min_vertex, const Point &max_vertex,
+       Texture_Material *material)
+      : min_vertex(min_vertex), max_vertex(max_vertex), material(material) {}
 
   std::optional<Intersection> intersect(const Ray &ray) override;
   Vector normal(const Point &point) override;
@@ -31,10 +32,11 @@ class AABB : public Object {
 
   Point min_vertex;
   Point max_vertex;
-  Vector normal_x_pos = Vector(1, 0, 0);
+  Texture_Material *material;
   Vector normal_x_neg = Vector(-1, 0, 0);
-  Vector normal_y_pos = Vector(0, 1, 0);
+  Vector normal_x_pos = Vector(1, 0, 0);
   Vector normal_y_neg = Vector(0, -1, 0);
-  Vector normal_z_pos = Vector(0, 0, 1);
+  Vector normal_y_pos = Vector(0, 1, 0);
   Vector normal_z_neg = Vector(0, 0, -1);
+  Vector normal_z_pos = Vector(0, 0, 1);
 };
