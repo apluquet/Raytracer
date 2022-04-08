56fbcc5bd0283f05aef933da0ce6abeac0948fdb
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-04-09 00:11:58 +0200

    [ADD] AABB intersect with normal

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include "utils/materials/cartoon.h"
 #include "utils/materials/phong.h"
 #include "utils/object_loader.h"
+#include "utils/objects/aabb.h"
 #include "utils/objects/sphere.h"
 #include "utils/objects/triangle.h"
 #include "utils/point.h"
@@ -26,26 +27,29 @@
 int main(int argc, char* argv[]) {
   // Create materials
   Color red(1., 0., 0.);
-  CartoonMaterial cartoon_material(red, 0.2, 0.5, 0.3, 100);
+  PhongMaterial phong_material(red, 0.2, 0.5, 0.3, 100);
 
   // Get object from .obj
   std::vector<std::string> files(argv + 1, argv + argc);
-  std::vector<Object*> objects = object_loader(files, &cartoon_material);
+  std::vector<Object*> objects = object_loader(files, &phong_material);
+
+  // Create AABB object
+  // AABB aabb(Point(0, 0, 0), Point(1, 1, 1), &phong_material);
 
   // Create scene (camera + light)
   // PointLight light(Point(4.1, -4.32, 3.36), Color(1., 1., 1.), 1);
-  PointLight light(Point(5, 5, 5), Color(1., 1., 1.), 1);
+  PointLight light(Point(2, 3, 5), Color(1., 1., 1.), 1);
   Image image(1920 / 2, 1080 / 2);
-  // Image image(480, 270);
   // Camera camera(Point(3.56, 1.67, 3.87), image, Vector(0, -1, 0),
   // Vector(0, 0, 1), 1, 60, 45);
-  Camera camera(Point(6, 0, 0), image, Vector(-1, 0, 0), Vector(0, 0, 1), 1, 60,
-                45);
+  Camera camera(Point(3, 3, 3), image, Vector(-1, -1, -1).normalize(),
+                Vector(-1, -1, 1).normalize(), 1, 60, 45);
   Scene scene(camera, 1.5, Color(0., 0., 0.));
   scene.addLight(&light);
+  // scene.addObject(&aabb);
 
   for (Object* object : objects) scene.addObject(object);
 
-  scene.render();
+  scene.render(false);
   scene.camera.image.to_ppm();
 }
